1f2cb108118b8aceb0d2b3a5a1c9ac1c367375eb
SebastianP08 <saguilarp@unbosque.edu.co>
2026-04-06 08:33:17 -0500

    Se han añadido multiples variables tanto de let como de const

---
+++ variables.js
@@ -0,0 +1,26 @@
+//Datos que ingresara el usuario
+let nombreCompleto = "";
+let edad = 0;
+let td= "";
+let numeroDeDocumento = "";
+
+let salario = 0;
+let comisiones = 0;
+let totalHorasExtra = 0;
+let nivelDeRiesgo = "";
+
+const ingresoBaseDeCotizacion = 0.70;
+const subsidioDeTransporte = 249095;
+const salarioMinimo = 1750905;
+const uvt = 52.37;
+
+const salud= 0.04;
+const pension= 0.04;
+const fondoDeSolidaridad = 0.01;
+
+const riesgoMinimo= 0.522;
+const riesgoBajo= 1.044;
+const riesgoMedio= 2.436;
+const riesgoAlto= 4.350;
+const riesgoMuyAlto= 6.960;
+
